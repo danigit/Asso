97026c7e971dd481516b6cbc2f3dc209cd61f877
danigit <ds.acconto@gmail.com>
2018-09-19 19:20:28 +0200

    implementata la visualizzazione delle attrezzature per contratto espandibile, aggiunto il numero insieme alla matricola di ogni attrezzatura. per le porte aggiunta l'ubicazione insieme al progressivo

--- www/js/get-attrezzature.js
+++ www/js/get-attrezzature.js
@@ -12,23 +12,21 @@ function getAttrezzature() {
             if (data.result) {
                 var contratto = '';
                 $.each(data[0], function (key, value) {
+                    var content = '';
                     //prendo i valori degli elementi ritornati ragruppati per contratto
                     $.each(value, function (innerKey, innerValue) {
                         //se la chiave e contratto mostro solo il label con il nome del contratto
                         if(innerKey === 'contratto') {
-                            var label = $('<a href="#" data-inset="false" class="margin-bottom-30 margin-top-50 fatture-header ui-shadow ui-btn ui-corner-all ui-header">' + innerValue + '</a>');
+                            content += "<div data-role='collapsible' data-inset='true'><h3>" + innerValue + "</h3>";
                             contratto = innerValue;
-                            $('#attrezzature-container').append(label);
                         }else {
                             //creo una lista per ogni contratto e inserisco i dati del contratto
-                            var lista = '<ul data-role="listview" data-inset="false">';
-
                             $.each(innerValue, function (lastKey, lastValue) {
                                 var name = lastValue.replace(/LISTA_/g, '').replace('_', ' ');
-                                lista += '<a href="#' + name.toLowerCase() + '" class="ui-btn" data-name="' + contratto + '">' + name + '</li>';
+                                content += '<a href="#' + name.toLowerCase() + '" class="ui-btn" data-name="' + contratto + '">' + name + '</li>';
                             });
-                            lista += '</ul>';
-                            $('#attrezzature-container').append(lista).trigger('create');
+                            content += '</div>';
+                            $('#attrezzature-container').append(content).trigger('create');
                         }
                     })
                 });
